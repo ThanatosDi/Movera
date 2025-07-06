ba7070eaafb700ada63edeaf66cba26f89caa305
ThanatosDi <yykkold55tw@gmail.com>
2025-07-07 00:04:05 +0800

    feat: 新增 FileEventHandler 類別以處理檔案系統事件

---
+++ app/event.py
@@ -0,0 +1,35 @@
+from queue import Queue
+
+from loguru import logger
+from watchdog.events import FileSystemEventHandler
+
+from .model import Config
+from .utils import wait_until_file_stable
+
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self, config: Config, queue: Queue):
+        self.config = config
+        self.queue = queue
+
+    def on_created(self, event):
+        if not event.is_directory:
+            if wait_until_file_stable(event.src_path) is True:
+                logger.info(f"Created: {event.src_path}")
+                self.queue.put(event.src_path)
+
+    def on_deleted(self, event):
+        if not event.is_directory:
+            logger.info(f"Deleted: {event.src_path}")
+
+    def on_modified(self, event):
+        if not event.is_directory:
+            logger.info(f"Modified: {event.src_path}")
+
+    def on_moved(self, event):
+        if not event.is_directory:
+            logger.info(f"檔案已寫入完成並關閉: {event.src_path}")
+
+    def on_closed(self, event):
+        if not event.is_directory:
+            logger.info(f"檔案已寫入完成並關閉: {event.src_path}")
