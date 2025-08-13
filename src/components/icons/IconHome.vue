11dcc2e98f5ad9459637e32d9c0f38530da72b95
ThanatosDi <yykkold55tw@gmail.com>
2025-08-13 20:52:05 +0800

    chore: 新增多個圖示元件，包括 IconAdd、IconCommunity、IconDocumentation、IconEcosystem、IconHome、IconSettings、IconSupport、IconTasks 和 IconTooling

---
+++ src/components/icons/IconHome.vue
@@ -0,0 +1,5 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955a1.5 1.5 0 012.122 0l8.954 8.955M21 12v9a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 21v-9" />
+  </svg>
+</template>
