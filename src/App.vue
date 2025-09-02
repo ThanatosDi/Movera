41f0f31ddc7a2bdcb4be4e1c6eea2f78dd6891de
ThanatosDi <yykkold55tw@gmail.com>
2025-09-02 22:53:13 +0800

    feat: 將 App.vue 的 script 語言從 js 變更為 ts，並新增 Toaster 的 duration 和 position 屬性

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
-<script setup lang="js">
+<script setup lang="ts">
 import { Toaster } from '@/components/ui/sonner';
+import { toastPosition } from '@/stores/sonnerStore'; // 一個全域 ref
 import { RouterView } from 'vue-router';
 import 'vue-sonner/style.css';
+
 </script>
 
 <template>
   <RouterView />
-  <Toaster richColors/>
+  <Toaster
+    :duration="10000"
+    richColors
+    :position="toastPosition"
+  />
 </template>
