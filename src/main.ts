4217858e75498028287f2fa7977dd49c2b7b0e2f
ThanatosDi <yykkold55tw@gmail.com>
2025-09-02 22:53:28 +0800

    feat: 新增 main.ts 和 vite-env.d.ts 檔案以初始化 Vue 應用程式

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,11 @@
-import './assets/style.css'
-
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
-
 import App from './App.vue'
+import './assets/style.css'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
