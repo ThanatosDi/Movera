2facf464eae5ce2ce645773f6ca8883a1b607b57
ThanatosDi <yykkold55tw@gmail.com>
2025-09-05 22:40:21 +0800

    feat: 增加 regex 範例

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -26,3 +26,30 @@ export const Messages = {
   DELETE_ERROR: '刪除失敗',
   REGEX_ERROR: '正規表達式格式無效',
 }
+
+export const RegexExamples = [
+  {
+    label: '動漫案例1', key: 'anime1',
+    filename: '公爵千金的家庭教師 - 01 [1080P][WEB-DL][AAC AVC][CHT].mp4',
+    src_filename: '公爵千金的家庭教師 - (\\d{2})(v2)? .+\\.mp4',
+    dst_filename: '公爵千金的家庭教師 - S01E\\1 [1080P][WEB-DL][AAC AVC][CHT].mp4'
+  },
+  {
+    label: '動漫案例2', key: 'anime2',
+    filename: '公爵千金的家庭教師 - 01v2 [1080P][WEB-DL][AAC AVC][CHT].mp4',
+    src_filename: '(.+) - (\\d{2}).+\\.mp4',
+    dst_filename: '\\1 - S01E\\2 [1080P][WEB-DL][AAC AVC][CHT].mp4'
+  },
+  {
+    label: '動漫案例3', key: 'anime3',
+    filename: '[Sakurato] Dan Da Dan (2025) [13][AVC-8bit 1080p AAC][CHT].mp4',
+    src_filename: '.+ Dan Da Dan \\(2025\\) \\[(\\d{2})\\]\\[AVC-8bit 1080p AAC\\]\\[CHT\\]\\.mp4',
+    dst_filename: '[Sakurato] Dan Da Dan (2025) [S02E\\1][AVC-8bit 1080p AAC][CHT].mp4'
+  },
+  {
+    label: '電影案例', key: 'movie',
+    filename: 'Movie.Title.2024.1080p.BluRay.x264-GROUP.mkv',
+    src_filename: '(.+)\\.(\\d{4})\\.1080p\\.BluRay\\.x264-(.+)\\.mkv',
+    dst_filename: '\\1 (\\2) [1080p BluRay x264-\\3].mkv'
+  },
+]
