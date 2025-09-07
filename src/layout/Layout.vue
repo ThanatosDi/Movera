9404e31bfdd247c646de3801466023ba729e0e53
ThanatosDi <yykkold55tw@gmail.com>
2025-09-07 17:02:55 +0800

    feat: RouteLink 使用 contents 中的常數

--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -3,6 +3,7 @@ import IconAdd from '@/components/icons/IconAdd.vue'
 import IconHome from '@/components/icons/IconHome.vue'
 import IconSettings from '@/components/icons/IconSettings.vue'
 import IconTasks from '@/components/icons/IconTasks.vue'
+import { Route } from '@/constants'
 import Header from '@/layout/Header.vue'
 import Sidebar from '@/layout/Sidebar.vue'
 import { useTaskStore } from '@/stores/taskStore'
@@ -31,28 +32,28 @@ onMounted(async () => {
     <footer class="sm:hidden bg-gray-800 p-2">
       <nav class="flex justify-around">
         <RouterLink
-          to="/"
+          :to="Route.HOME"
           class="flex flex-col items-center text-gray-400 hover:text-white"
         >
           <IconHome class="size-6" />
           <span class="text-xs">首頁</span>
         </RouterLink>
         <RouterLink
-          to="/tasks"
+          :to="Route.TASKS"
           class="flex flex-col items-center text-gray-400 hover:text-white"
         >
           <IconTasks class="size-6" />
           <span class="text-xs">任務</span>
         </RouterLink>
         <RouterLink
-          to="/create"
+          :to="Route.CREATE_TASK"
           class="flex flex-col items-center text-gray-400 hover:text-white"
         >
           <IconAdd class="size-6" />
           <span class="text-xs">新增</span>
         </RouterLink>
         <RouterLink
-          to="/settings"
+          :to="Route.SETTING"
           class="flex flex-col items-center text-gray-400 hover:text-white"
         >
           <IconSettings class="size-6" />
