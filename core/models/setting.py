d71d3281aaef208d60321e03f6875e2a65a101b3
ThanatosDi <yykkold55tw@gmail.com>
2025-09-08 22:45:44 +0800

    feat: 增加 setting model

---
+++ core/models/setting.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String
+
+from core.database import Base
+
+
+class Setting(Base):
+    __tablename__ = "setting"
+
+    key = Column(String, primary_key=True)
+    value = Column(String)
+    description = Column(String)
