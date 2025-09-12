5a26f3f0abbb437618113ef40532570524a241c8
ThanatosDi <yykkold55tw@gmail.com>
2025-09-12 23:15:04 +0800

    feat: vue app 建立時馬上取得 setting 作為環境變數、初始化 i18n

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
+import { useSettingStore } from '@/stores/settingStore'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
+import i18n from './locales'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(i18n)
+
+const settingStore = useSettingStore(pinia)
+settingStore.fetchSettings()
+
 app.mount('#app')
