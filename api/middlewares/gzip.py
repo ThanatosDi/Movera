6eb49435f946eb9de0039378275d456f831c5a06
ThanatosDi <yykkold55tw@gmail.com>
2025-09-16 11:27:27 +0800

    docs: 調整註解

--- api/middlewares/gzip.py
+++ api/middlewares/gzip.py
@@ -4,7 +4,7 @@ from fastapi.middleware.gzip import GZipMiddleware
 
 def setup_gzip(app: FastAPI):
     """
-    設定 FastAPI 應用程式的 CORS (跨來源資源共用) 中介軟體。
+    設定 FastAPI 應用程式的 GZIP (壓縮) 中介軟體。
     """
     app.add_middleware(
         GZipMiddleware,
