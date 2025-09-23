fb0822b9c75375dd653c6ea8ae48fd7b8693e466
ThanatosDi <yykkold55tw@gmail.com>
2025-09-23 14:22:02 +0800

    feat(pwa): add PWA support
    新增 vite-plugin-pwa 以啟用漸進式網頁應用程式 (PWA) 功能，並更新 .gitignore 以排除相關的建置產物。

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 import path from 'node:path'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 const packageJson = fs.readFileSync(path.resolve(__dirname, './package.json'), 'utf-8')
@@ -18,6 +19,30 @@ export default defineConfig({
       compositionOnly: false,
       runtimeOnly: false,
       include: path.resolve(__dirname, './src/locales/*.json'),
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,vue,txt,woff2}'],
+      },
+      manifest: {
+        name: 'Movera',
+        short_name: 'Movera',
+        description: 'Movera is a simple media server for your home.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
