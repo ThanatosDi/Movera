02975667f38642cf8a598cf8f2af31335006415b
ThanatosDi <yykkold55tw@gmail.com>
2026-03-10 20:01:39 +0800

    chore: 升級版本至 3.2.1

--- backend/backend.py
+++ backend/backend.py
@@ -47,7 +47,7 @@ app = FastAPI(
     lifespan=lifespan,
     title="Movera API",
     description="API for managing file moving and renaming tasks.",
-    version="3.2.0",
+    version="3.2.1",
 )
 
 
